a17c8ede1acaa7034b3357bbc74a9cc93e1794e3
JoelDevSpace <joel_jmg@hotmail.com>
2025-06-20 11:50:31 +0200

    Mise en place d'un layout - creation 1er composant item-link - Ajout route project

---
+++ resources/views/Components/link-item.blade.php
@@ -0,0 +1,6 @@
+@props(['active' => false])
+
+<li>
+    <a {{ $attributes->class(['hover:text-gray-600', 'font-bold' => $active]) }}>{{ $slot }}
+    </a>
+</li>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('homepage');
+})->name('homepage');
+
+Route::get('/project', function () {
+    return view('project');
+})->name('project');
